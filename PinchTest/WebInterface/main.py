cddb66ef88497fd732c2a4dcc732be56dc854004
georgew <wgeorge@outlook.com>
2017-06-14 22:25:27 -0700

    started oauth stuff with python library

--- PinchTest/WebInterface/main.py
+++ PinchTest/WebInterface/main.py
@@ -1,35 +1,28 @@
 import sys
-from oauth2client import client
-from googleapiclient import 
+import httplib2
+import webapp2
+import os
+import urllib
+import urllib2
+import uuid
+import json
+from goauthcred import GoogleOAuth
+from apiclient.discovery import build
+from oauth2client import tools
+from oauth2client.file import Storage
+from oauth2client.client import AccessTokenRefreshError
+from oauth2client.client import OAuth2WebServerFlow
 
-def main(argv):
-	 # Authenticate and construct service.
-  service, flags = sample_tools.init(
-      argv, 'plus', 'v1', __doc__, __file__,
-      scope='https://www.googleapis.com/auth/plus.me')
+google_creds = GoogleOAuth()
+client_id = google_creds.clientID()
+client_secret = google_creds.clientSecret()
+scope = google_creds.scope()
+auth_end = google_creds.authEnd()
+token = google_creds.token()
+api = google_creds.api_key()
 
-  try:
-    person = service.people().get(userId='me').execute()
+flow = OAuth2WebServerFlow(client_id, client_secret, scope)
 
-    print('Got your ID: %s' % person['displayName'])
-    print()
-    print('%-040s -> %s' % ('[Activitity ID]', '[Content]'))
+storage = Storage('credentials.dat')
 
-    # Don't execute the request until we reach the paging loop below.
-    request = service.activities().list(
-        userId=person['id'], collection='public')
-
-    # Loop over every activity and print the ID and a short snippet of content.
-    while request is not None:
-      activities_doc = request.execute()
-      for item in activities_doc.get('items', []):
-        print('%-040s -> %s' % (item['id'], item['object']['content'][:30]))
-
-      request = service.activities().list_next(request, activities_doc)
-
-  except client.AccessTokenRefreshError:
-    print ('The credentials have been revoked or expired, please re-run'
-      'the application to re-authorize.')
-
-if __name__ == '__main__':
-  main(sys.argv)
+credentials = storage.get()
