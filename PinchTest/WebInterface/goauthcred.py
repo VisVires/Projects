b56dd6605a0b123c1bbc4958af31fe53fdf1bec1
georgew <wgeorge@outlook.com>
2017-06-14 19:36:53 -0700

    added class for oauth of web app

---
+++ PinchTest/WebInterface/goauthcred.py
@@ -0,0 +1,15 @@
+class GoogOAuth():
+	def clientID(self):
+		return "893397818126-9r3dghaseltqhu39rp0a1dga1jdaki2b.apps.googleusercontent.com"
+
+	def clientSecret(self):
+		return "O7B1fDpqR7F9pWQmyUr_SbTT"
+
+	def scope(self):
+		return "profile"
+
+	def authEnd(self):
+		return "https://accounts.google.com/o/oauth2/v2/auth"
+
+	def token(self):
+		return "https://www.googleapis.com/oauth2/v4/token" 
---
+++ PinchTest/WebInterface/test.py
@@ -0,0 +1,5 @@
+from goauthcred import GoogOAuth
+
+googOAuth = GoogOAuth()
+print "The Client secret is " + googOAuth.clientID()
+
