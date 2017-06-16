ca3468b6de7a39a6d18e415e01423d2c1f7904fc
georgew <wgeorge@outlook.com>
2017-06-16 09:32:52 -0700

    added backend to web app

--- PinchTest/WebInterface/main.py
+++ PinchTest/WebInterface/main.py
@@ -1,4 +1,5 @@
 from oauth2client.client import OAuth2WebServerFlow
+from google.appengine.ext.webapp import template
 from goauthcred import GoogleOAuth
 import httplib2
 import logging
@@ -28,31 +29,25 @@ class MainPage(webapp2.RequestHandler):
 		code = self.request.get("code")
 		if not code:
 			auth_uri = flow.step1_get_authorize_url()
-			self.response.write("""\<html><title>Crozdesk Python Example</title><body><a href="%s">Click here to login with Crozdesk.
-				</a></body></html>""" % auth_uri)
+			template_values = {'auth_uri': auth_uri}
+			self.response.out.write((template.render('oauth.html', template_values)))
 		else:
 			credentials = flow.step2_exchange(code)
 			http = httplib2.Http()
 			http = credentials.authorize(http)
 			response, content = http.request(URL, "GET")
 			oauth_data = json.loads(content)
-			self.response.write(oauth_data['id'])
+			user_id = oauth_data['id']
+			body_fat_url = 'https://bodyfatpinchtest.appspot.com/user/' + user_id
+			template_values = {'url': body_fat_url}
+			self.response.out.write(template.render('index.html', template_values))
 
 
 app = webapp2.WSGIApplication([
-    ('/.*', MainPage)
+    ('/', MainPage)
 ], debug=True)
 
 '''
 ('/outh2callback', OAuthHandler),
 ('/login', LogInHandler)
-
-def get(self):
-		google_creds = GoogleOAuth()
-		client_id = google_creds.clientID()
-		client_secret = google_creds.clientSecret()
-		scope = google_creds.scope()
-		auth_end = google_creds.authEnd()
-		token = google_creds.token()
-		api = google_creds.api_key()
 '''
